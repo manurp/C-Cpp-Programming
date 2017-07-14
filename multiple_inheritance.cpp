26bcd7294a6a7e56b0887559d5bfb91188542917
manurp <manojrpoojary@gmail.com>
2017-07-14 22:37:37 +0530

    Correction of the code

--- multiple_inheritance.cpp
+++ multiple_inheritance.cpp
@@ -9,14 +9,15 @@ public:
 	B1(int i,int d):i(i),data(d) {cout<<"B1::B1()\n";}
 	void f(int) {cout<<"B1::f(int)\n";}
 	void g() {cout<<"B1::g()\n";}
-	~B1() {cout<<"B1::~B1()\n";
+	~B1() {cout<<"B1::~B1()\n";}
 };
 
 class B2
-{protected:
-	int j , int data;
+{
+protected:
+	int j,data;
 public:
-	B2(int j=0,data=0):j(j),data(data) {cout<<"B2::B2()\n";}
+	B2(int j=0,int data=0):j(j),data(data) {cout<<"B2::B2()\n";}
 	~B2() {cout<<"B2::~B2()\n";}
 	void f(int) {cout<<"B2::f(int)\n";}
 	void g(int) {cout<<"B2::g(int)\n";}
@@ -29,13 +30,15 @@ public:
 	//To access data use B1::data or B2::data
 	D(int x,int y,int z):B1(x,y),k(z) {cout<<"D::D()\n";}
 	~D() {cout<<"D::~D()\n";}
+	using B1::f;
+	using B2::g;
 };
 
 int main() {
 	D d(1,2,3);
 
-	d.f(5);
-	d.g();
+    d.f(5);
+	d.B1::g();
 	d.g(3);
 
 	return 0;
