60b9627fa1d09397ae16801206ed89e79e8b1365
manurp <manojrpoojary@gmail.com>
2017-07-18 19:33:34 +0530

    Simple code to demonstrate the exception flow in c++

---
+++ exception_flow.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class MyException: public exception{};
+class Myclass{};
+
+void h() {
+	Myclass a;
+	//throw 1;
+	 //throw 2.5;
+	//throw MyException();
+	// throw exception();
+	// throw Myclass();
+}
+
+void g() {
+	Myclass a;
+	try {
+		h();
+	}
+	catch(int) {cout<<"int\n";}
+	catch(double) {cout<<"double\n";}
+	catch(...) {throw;}
+}
+
+void f() {
+	Myclass a;
+	try {
+		g();
+	}
+	catch(MyException) {cout<<"MyException\n";}
+	catch(exception){ cout<<"exception\n";}
+	catch(...) {throw;}
+}
+
+int main() {
+	try {
+		f();
+	}
+	catch(...) {cout<<"Unknown\n";}
+
+	return 0;
+}
