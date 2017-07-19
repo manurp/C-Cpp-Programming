39800d930fffbbbb2cbb9ca8e1c7af9918d630a8
manurp <manojrpoojary@gmail.com>
2017-07-19 11:19:53 +0530

    Correction of sematic

--- swap_using_template.cpp
+++ swap_using_template.cpp
@@ -6,14 +6,15 @@ template <class T>
 inline void mswap(T&i,T& j) {
 	T temp=i;
 	i=j;
-	j=i;
+	j=temp;
 }
-
 
 int main()
 {
 	int i=5,j=10;
 	double x=5.6,y=6.5;
+	string s1 = "hi",s2 = "hello";
+
 	cout<<"Inputs: "<<i<<", "<<j<<endl;
 	mswap(i,j);
 	cout<<"Outputs: "<<i<<", "<<j<<endl;
@@ -24,6 +25,9 @@ int main()
 	cout<<"Complex inputs: "<<r<<", "<<s<<endl;
 	mswap(r,s);
 	cout<<"Complex outputs: "<<r<<", "<<s<<endl;
+    cout<<"String inputs: "<<s1<<", "<<s2<<endl;
+	mswap(s1,s2);
+	cout<<"Complex outputs: "<<s1<<", "<<s2<<endl;
 
 	return 0;
 }
