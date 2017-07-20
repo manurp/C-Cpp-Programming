5c103afa7a2f7c3ae32f7605d61ad4e98ce488ae
manurp <manojrpoojary@gmail.com>
2017-07-20 16:14:22 +0530

    Template stack with forward declaration demo

---
+++ template-stack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+template <class T> class Stack;
+
+void reverseString(Stack<char>&,char*);
+
+template <class T>
+class Stack {
+	T data_[100];
+	int top_;
+public:
+	Stack():top_(-1){} ~Stack(){}
+
+	void push(T d){data_[++top_] = d;}
+	void pop(){--top_;}
+	const T& top(){return data_[top_];}
+	bool empty() const {return top_==-1;}
+};
+
+int main(int argc, char const *argv[])
+{
+	Stack<char> s;
+	char* str = "ABCDE";
+
+	reverseString(s,str);
+
+	return 0;
+}
+
+void reverseString(Stack<char>&s,char* str) {
+	for(unsigned int i=0;i<strlen(str);i++) s.push(str[i]);
+        cout<<"Input string: "<<str<<endl;
+		cout<<"Reversed string: ";
+		while(!s.empty()){
+			cout<<s.top();
+			s.pop();
+		}
+}
