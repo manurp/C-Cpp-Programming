aa4ea4a186e3a7e7aa2604e56345228a0439033f
manurp <manojrpoojary@gmail.com>
2017-07-25 17:13:50 +0530

    Alternate way

--- files/file_cpp.cpp
+++ files/file_cpp.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main() {
 	ofstream fileobj("file.txt");
+	/* Can be done in the following way
+	ofstream fileobj;
+	fileobj.open("simefilename");
+	fileobj<<"some txt";
+	fileobj.close();
+	*/
 	if(fileobj.is_open()) {
 		fileobj << "The first file created using fstream in c++\n"<<endl;
 	} 
