ca68d74b6203fadc2a5f2940446ec0d20be18956
manurp <manojrpoojary@gmail.com>
2017-07-30 20:43:29 +0530

    Some additional comments

--- qsort.c
+++ qsort.c
@@ -2,16 +2,16 @@
 #include<stdlib.h>
 
 int comp(const void* a,const void* b){
-	return(*(int*)a<*(int*)b);
+	return(*(int*)a < *(int*)b); //Change the sign(>) to sort in ascending order
 }
 
 int main()
 {
 	int data[]={1,2,3,4,5};
-
+    int i;
 	qsort(data,5,sizeof(int),comp);
 
-	for(int i=0;i<5;i++){
+	for(i=0;i<5;i++){
 		printf("%d ",data[i]);
 	}
 	return 0;
