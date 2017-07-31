3dad67d123a2e2f7eb1a1ecfec1c43116b99a325
manurp <manojrpoojary@gmail.com>
2017-07-31 12:13:54 +0530

    Code implementing insertion sort algorithm

---
+++ sorting/insertion_sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void insertion_sort(int* A, int n) {
+    int i, value, hole;
+    for(i = 1; i < n; i++) {
+        value = A[i];
+        hole = i;
+        while(hole > 0 && A[hole - 1] > value) {
+            A[hole] = A[hole - 1];
+            hole--;
+        }
+        A[hole] = value;
+    }
+}
+
+int main() {
+    int a[] = {1,12,4,5,8,6,7,10,0,2};
+    int i;
+    insertion_sort(a,10);
+    for(i = 0; i<10 ; i++) printf("%d ",a[i]);
+    return 0;
+}
